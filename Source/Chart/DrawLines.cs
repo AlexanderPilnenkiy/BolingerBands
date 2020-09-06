3fa2f0b46d2fd62a2f65c961d2f7e7193f14081f
Alexander <pi.16b.alex@gmail.com>
2020-09-06 18:01:26 +0300

    Middle line added

--- Source/Chart/DrawLines.cs
+++ Source/Chart/DrawLines.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
@@ -13,22 +14,27 @@ namespace BolingerBands.Source.Chart
 {
     class DrawLines
     {
-        public void Paint(MainWindow mainWindow, Designations designations)
+        public async void Paint(MainWindow mainWindow, Designations designations)
         {
             double Range = designations.Max - designations.Min;
             int FieldHeight = Settings.ChartHeight - 35;
             for (int i = 0; i < designations.High.Count - 1; i++)
             {
-                double Y1High = FieldHeight - FieldHeight / (Range / (designations.High[i] - designations.Min));
-                double Y2High = FieldHeight - FieldHeight / (Range / (designations.High[i + 1] - designations.Min));
-                DrawLine(mainWindow, i, i + 1, Y2High, Y1High, Brushes.Red);
-                double Y1Low = FieldHeight - FieldHeight / (Range / (designations.Low[i] - designations.Min));
-                double Y2Low = FieldHeight - FieldHeight / (Range / (designations.Low[i + 1] - designations.Min));
-                DrawLine(mainWindow, i, i + 1, Y2Low, Y1Low, Brushes.Green);
+                double Y1High = FieldHeight - FieldHeight / (Range / (designations.High[i + 1] - designations.Min));
+                double Y2High = FieldHeight - FieldHeight / (Range / (designations.High[i] - designations.Min));
+                await DrawLine(mainWindow, i, i + 1, Y1High, Y2High, Brushes.Red);
+                double Y1Low = FieldHeight - FieldHeight / (Range / (designations.Low[i + 1] - designations.Min));
+                double Y2Low = FieldHeight - FieldHeight / (Range / (designations.Low[i] - designations.Min));
+                await DrawLine(mainWindow, i, i + 1, Y1Low, Y2Low, Brushes.Green);
+                double Y1Middle = FieldHeight - FieldHeight / (Range / (designations.High[i + 1] - (designations.High[i + 1] - designations.Low[i + 1]) 
+                    / 2 - designations.Min));
+                double Y2Middle = FieldHeight - FieldHeight / (Range / (designations.High[i] - (designations.High[i] - designations.Low[i])
+                    / 2 - designations.Min));
+                await DrawLine(mainWindow, i, i + 1, Y1Middle, Y2Middle, Brushes.Blue);
             }
         }
 
-        public void DrawLine(MainWindow mainWindow, int X1, int X2, double Y1, double Y2, Brush brushes)
+        public async Task<int> DrawLine(MainWindow mainWindow, int X1, int X2, double Y1, double Y2, Brush brushes)
         {
             Line line = new Line()
             {
@@ -39,6 +45,7 @@ namespace BolingerBands.Source.Chart
                 Stroke = brushes
             };
             mainWindow.meshGrid.Children.Add(line);
+            return 0;
         }
     }
 }
--- Source/Legend/Designations.cs
+++ Source/Legend/Designations.cs
@@ -22,35 +22,43 @@ namespace BolingerBands.Source.Legend
 
         public void ExportExcel()
         {
-            OpenFileDialog openDialog = new OpenFileDialog();
-            openDialog.DefaultExt = "*.xls;*.xlsx";
-            openDialog.Filter = "файл Excel (Spisok.xlsx)|*.xlsx";
-            openDialog.Title = "Загрузка файла с данными";
-            if (!(openDialog.ShowDialog() == DialogResult.OK)) { }
-            var workbook = new XLWorkbook(openDialog.FileName);
-            var worksheet = workbook.Worksheet(1);
-            var rows = worksheet.RangeUsed().RowsUsed();
-            foreach (var row in rows)
+            try
             {
-                Time.Add(row.Cell(1).Value.ToString());
-                Open.Add(Convert.ToInt32(row.Cell(2).Value));
-                High.Add(Convert.ToInt32(row.Cell(3).Value));
-                Low.Add(Convert.ToInt32(row.Cell(4).Value));
-                Close.Add(Convert.ToInt32(row.Cell(5).Value));
+                OpenFileDialog openDialog = new OpenFileDialog();
+                openDialog.DefaultExt = "*.xls;*.xlsx";
+                openDialog.Filter = "файл Excel (Spisok.xlsx)|*.xlsx";
+                openDialog.Title = "Загрузка файла с данными";
+                if (!(openDialog.ShowDialog() == DialogResult.OK)) { }
+                var workbook = new XLWorkbook(openDialog.FileName);
+                var worksheet = workbook.Worksheet(1);
+                var rows = worksheet.RangeUsed().RowsUsed();
+                foreach (var row in rows)
+                {
+                    Time.Add(row.Cell(1).Value.ToString());
+                    Open.Add(Convert.ToInt32(row.Cell(2).Value));
+                    High.Add(Convert.ToInt32(row.Cell(3).Value));
+                    Low.Add(Convert.ToInt32(row.Cell(4).Value));
+                    Close.Add(Convert.ToInt32(row.Cell(5).Value));
+                }
+                Max = High.Max();
+                Min = Low.Min();
+                GetRightLegend();
             }
-            Max = High.Max();
-            Min = Low.Min();
-            GetRightLegend();
+            catch { }
         }
 
         public void GetRightLegend()
         {
-            for (int i = Min; i < Max; i += ((Max - Min) / 9))
+            try
             {
-                RightLegend.Add(i);
+                for (int i = Min; i < Max; i += ((Max - Min) / 9))
+                {
+                    RightLegend.Add(i);
+                }
+                RightLegend[9] = Max;
+                RightLegend.Reverse();
             }
-            RightLegend[9] = Max;
-            RightLegend.Reverse();
+            catch { }
         }
     }
 }
